76283cbd08f2864a542752efae3fed8a2fc46bf9
itamarpambo <itamarpambo751@gmail.com>
2024-01-19 21:21:21 +0100

    Feat: navigation buttons added

--- src/components/ScrollableList.jsx
+++ src/components/ScrollableList.jsx
@@ -2,14 +2,21 @@ import styled from "styled-components"
 
 import { Availables } from "../data/availables"
 import { AvailablePeople } from "."
+import { ArrowLeft, ArrowRight } from "phosphor-react"
 
 const arrayItems = Availables
 export const ScrollableList = () => {
     return(
         <ScrollableListItems>
-            <HorizontalScrollableContainer itemsCount={arrayItems.length}>
+            <ScrollablePointer>
+                <ArrowLeft />
+            </ScrollablePointer>
+            <HorizontalScrollableContainer count={arrayItems.length}>
                 {arrayItems.map((user) => <AvailablePeople key={user.identity} {...user}/>)}
             </HorizontalScrollableContainer>
+            <ScrollablePointer>
+                <ArrowRight />
+            </ScrollablePointer>
         </ScrollableListItems>
     )
 }
@@ -18,11 +25,25 @@ const ScrollableListItems = styled.ul`
     margin: 0 auto;
     overflow-x: hidden;
     transform: translateY(100px);
+    position: relative;
 `
-const HorizontalScrollableContainer = styled.div(({ itemsCount }) => `
-    width: ${(itemsCount * 500) + 'px'};
+const HorizontalScrollableContainer = styled.div((properties) => `
+    width: ${(properties.count * 500) + 'px'};
     div { &:not(:first-child) { margin-left: 40px } }
 `)
 const ScrollablePointer = styled.button`
-    
+    position: absolute;
+    top: 50%;
+    &:first-child { 
+        left: 0px; 
+    }
+    &:last-child { 
+        right: 0;
+    }
+    border-radius: 30px;
+    height: 60px;
+    width: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
